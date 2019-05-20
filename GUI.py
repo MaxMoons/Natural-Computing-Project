78fd08246d56af20ee2bfa21475d3364e5c1d9d7
MaxMoons <Max_Moons@Hotmail.com>
2019-05-20 16:29:39 +0200

    dingen
    ..

--- GUI.py
+++ GUI.py
@@ -42,17 +42,19 @@ class GUI(Frame):
         self.frame_width, self.frame_height = 600, 100
         self.frame = Frame(self.root, bg='grey')
         self.frame.pack(fill='x')
+        Label(self.frame, text="Mode:", bg='grey').pack(side='left')
+        self.modelabel = Label(self.frame, text="Default", bg='grey').pack(side='left')
         self.air_button = Button(self.frame, text='Delete')
-        self.air_button.pack(side='left', padx = 10)
+        self.air_button.pack(side='left', padx=10)
         self.water_button = Button(self.frame, text='Water')
-        self.water_button.pack(side='left', padx = 10)
+        self.water_button.pack(side='left', padx=10)
         self.stone_button = Button(self.frame, text='Stone')
-        self.stone_button.pack(side='left', padx = 10)
+        self.stone_button.pack(side='left', padx=10)
         
         self.start_button = Button(self.frame, text='Start simulation')
-        self.start_button.pack(side='left', padx = 60)
+        self.start_button.pack(side='left', padx=60)
         self.stop_button = Button(self.frame, text='Stop simulation')
-        self.stop_button.pack(side='left', padx = 10)
+        self.stop_button.pack(side='left', padx=10)
         
         self.frame2 = Frame(self.root)
         self.frame2.pack(fill='x')
@@ -72,10 +74,10 @@ class GUI(Frame):
         self.Parameter2.pack(padx=10,side='left')
         self.Parameter2.insert(0, "0")
         self.w = Canvas(self.root, width=self.canvas_width, height=self.canvas_height)
+        self.w.bind("<Button-1>", leftclick)
         self.w.pack()
         
         self.create_grid()
-
     
     '''
     Deze functie heb ik van internet gehaald om een grid te tekenen
@@ -84,7 +86,7 @@ class GUI(Frame):
     '''
     def create_grid(self):
         # vertical lines at an interval of "line_distance" pixel
-        for x in range(self.particle_size,self.canvas_width,self.particle_size):
+        for x in range(self.particle_size, self.canvas_width, self.particle_size):
             self.w.create_line(x, 0, x, self.canvas_height, fill="gray80", width=0.15)
         # horizontal lines at an interval of "line_distance" pixel
         for y in range(self.particle_size, self.canvas_height, self.particle_size):
@@ -92,13 +94,14 @@ class GUI(Frame):
 
     '''
     This function is called to draw a particle in the given coordinate when redrawing the entire grid
-    canvas is the space the particle should be drawn in
+    It should be called from the mousehandler
+    canvas is the space the particle should be drawn in.
     n is the type of particle that should be drawn (i.e. 0=air, 1=water, 2=stone)
     x, y are x and y coordinates (x1,y1) of the pixel
     pixel_size is the fixed pixel size that is used to determine x2 and y2
     tkinter colors: http://www.science.smith.edu/dftwiki/index.php/Color_Charts_for_TKinter
     '''
-    def draw_particle(self, particle_type, x1, y1):
+    def draw_particle(self, x1, y1):
         # Gebruik self.particle size om uit x1,y1 de x2,y2 af te leiden
         # 2 = stone
         # A solid filled dark-gray rectangle (gray40?)
@@ -123,7 +126,7 @@ class GUI(Frame):
     Het hele grid steeds opnieuw tekenen kan wat omslachtig zijn; misschien iets bedenken waarbij alleen de veranderde
     getallen opnieuw getekend worden (e.g. coordinatenstelsel van vorige iteratie meegeven en kijken of getal hetzelfde is?)
     '''
-    def draw_grid(self, coords, particle_size):
+    def draw_grid(self, coords):
         for y in range(self.canvas_height):
             for x in range(self.canvas_width):
                 self.draw_particle(coords[y][x], x*self.particle_size, y*self.particle_size)
@@ -136,6 +139,18 @@ class GUI(Frame):
 
     def stone_button_click(self):
         self.mode = 2
+
+    def change_label(self):
+        data = self.air_button
+        self.entryLabel.configure(text=data)
+
+
+def leftclick(self,eventorigin):
+    global x0, y0
+    x0 = eventorigin.x
+    y0 = eventorigin.y
+    print(x0, y0)
+    print("left")
 
 
 if __name__ == "__main__":
